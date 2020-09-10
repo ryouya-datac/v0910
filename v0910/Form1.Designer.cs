cde57bcd9f1eebb170cd2f67209827828119b239
pizza_girl <pizza.girl.dat@gmail.com>
2020-09-10 09:35:49 +0900

    復習

--- v0910/Form1.Designer.cs
+++ v0910/Form1.Designer.cs
@@ -28,13 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(583, 390);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +48,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
 
     }
 }
--- v0910/Form1.cs
+++ v0910/Form1.cs
@@ -12,9 +12,32 @@ namespace v0910
 {
     public partial class Form1 : Form
     {
+        int[] vx = new int[3];
+        int[] vy = new int[3];
+        Label[] labels = new Label[3];
+        static Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            for(int i =0;i<3;i++)
+            {
+                vx[i] = rand.Next(-15, 16);
+                vy[i] = rand.Next(-15, 15);
+
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "★";
+                Controls.Add(labels[i]);
+
+                labels[i].Left = rand.Next(ClientSize.Width - labels[i].Width);
+                labels[i].Top = rand.Next(ClientSize.Height - labels[i].Height);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
         }
     }
 }
